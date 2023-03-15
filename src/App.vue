931037bfbdd438dab5a0ac30afb610ca97ddf479
seeun jeong <sesej4512@gmail.com>
2023-03-15 15:11:08 +0900

    feat: vue.js first setting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">
+    {{greet}}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+ setup(){
+  const name = "kosa!";
+  const greeting = (name) =>{
+    return 'Hello' + name;
   }
+ 
+  const greet = greeting(name);
+
+  return{
+    name,
+    greeting,
+    greet,
+  
+  }
+
+
+
+ }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .name{
+    color:red;
+  }
 </style>
