b9c765e5472ef25b7da8f8291c2d4ee27cf234b7
seeun jeong <sesej4512@gmail.com>
2023-03-16 10:52:10 +0900

    TodoSimpleForm component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,32 @@
 <template>  
   <div class="container">
-    <h2>To-Do</h2>
-    <form       
-      @submit.prevent="onSubmit"  
-    >
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control" 
-            type="text" 
-            v-model="todo"
-            placeholder="Type new to-do"
-          >
-        </div> 
-        <div>
-          <button
-            class="btn btn-primary"
-            type="submit"
-          >
-            Add
-          </button>
-        </div> 
-      </div>
-      <div v-if="hasError" style="color: red">
-        This is cannot be empty  
-      </div>        
-    </form>  
+    <h2>To-Do List</h2>
+    <TodoSimpleForm />
+    
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
     <div
-      v-for="todo in todos"
+      v-for="(todo,index) in todos"
       :key="todo.id"
       class="card mt-2">
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input class="form-check-input"
-                type="checkbox">
-          <label class="form-check-label">
+                type="checkbox"
+                v-model="todo.completed">
+          <label class="form-check-label"
+                 :class="{todo: todo.completed}">
             {{ todo.subject }}
-          </label>
-        </div>
+          </label>             
+        </div>      
+        <div>
+            <button 
+              class="btn btn-danger btn-sm"
+              @click="deleteTodo(index)">
+              Delete
+            </button>  
+        </div>        
       </div>
     </div>    
   </div>   
@@ -45,40 +34,45 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 export default {
+  components: {
+    TodoSimpleForm
+  },
   setup(){
-    const todo = ref('');  
-    const todos = ref([]); 
-    const hasError = ref(false);    
-
-    const onSubmit = () =>{  
-      if(todo.value == ''){
-        hasError.value = true;
-      }else{
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false,
-        });
-        hasError.value = false;
-        todo.value ='';
-      }          
       
-    }
-   
+    const todos = ref([]);     
+       
+
+    // const onSubmit = () =>{  
+    //   if(todo.value == ''){
+    //     hasError.value = true;
+    //   }else{
+    //     todos.value.push({
+    //       id: Date.now(),
+    //       subject: todo.value,
+    //       completed: false,  
+    //     });
+    //     hasError.value = false;
+    //     todo.value = '';
+    //   } 
+    // }
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    }   
     
-    return{
-      todo,      
-      onSubmit,
-      todos,
-      hasError,
+    return{      
+      todos,      
+      deleteTodo,
     }
   }  
 }
 </script>
 
-<style>
-  .name{
-    color: red;
+<style>  
+  .todo{
+    color: gray;
+    text-decoration: line-through;
   }
 </style>
