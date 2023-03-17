63129dcaacacc4d17e17566599e8e6cf8a319607
seeun jeong <sesej4512@gmail.com>
2023-03-17 13:00:40 +0900

    feat: TodoList 중간 완성

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,12 +3,16 @@
         v-for="(todo,index) in todos"
         :key="todo.id"
         class="card mt-2">
-        <div class="card-body p-2 d-flex align-items-center">
-            <div class="form-check flex-grow-1">
+        <div class="card-body p-2 d-flex align-items-center"
+             @click="moveToPage(todo.id)">
+            <div 
+                
+                class="form-check flex-grow-1">
                 <input class="form-check-input"
                         type="checkbox"
                         :checked="todo.completed"
-                        @change="toggleTodo(index)">
+                        @change="toggleTodo(index, $event)"
+                        @click.stop>
                 <label class="form-check-label"
                         :class="{todo: todo.completed}">
                     {{ todo.subject }}
@@ -17,7 +21,7 @@
             <div>
                 <button 
                     class="btn btn-danger btn-sm"
-                    @click="deleteTodo(index)">
+                    @click.stop="deleteTodo(index)">
                     Delete
                 </button>  
             </div>        
@@ -26,6 +30,7 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 export default {
     props: {
         todos:{
@@ -34,18 +39,32 @@ export default {
         }
     },
     emits: ['toggle-todo', 'delete-todo'],
-    setup(props, {emit}){
-        const toggleTodo = (index) => {
-            emit('toggle-todo', index);
+    setup(props, {emit}){  
+        const router = useRouter();     
+
+        const toggleTodo = (index, event) => {
+            emit('toggle-todo', index, event.target.checked);
         }
 
         const deleteTodo = (index) => {
             emit('delete-todo', index);
         }
 
+        const moveToPage = (todoId) => {
+            console.log(todoId);
+           // router.push('/todos/' + todoId);
+           router.push({
+            name: 'Todo',
+            params: {
+                id: todoId
+            }
+           });
+        }
+
         return {
             toggleTodo,
             deleteTodo,
+            moveToPage,
         }
     }   
 }
--- src/pages/todos/index.vue
+++ src/pages/todos/index.vue
@@ -1,175 +1,174 @@
 <template>    
-    <router-view/>
-      <div class="container">
-        <h2>To-Do List</h2>
-        <input
-          class="form-control" 
-          type="text" 
-          v-model="searchText"
-          placeholder="Search"
-          @keyup.enter="searchTodo"
-        >
-        <hr>
-        <TodoSimpleForm @add-todo="addTodo"/>
-        <div style="color: red">{{error}}</div>
-        
-        <div v-if="!todos.length">
-          추가된 Todo가 없습니다.
-        </div>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <input
+      class="form-control" 
+      type="text" 
+      v-model="searchText"
+      placeholder="Search"
+      @keyup.enter="searchTodo"
+    >
+    <hr>
+    <TodoSimpleForm @add-todo="addTodo"/>
+    <div style="color: red">{{error}}</div>
     
-        <div v-if="!todos.length">
-          There is noting to display  
-        </div>    
-    
-        <TodoList :todos="todos"
-                  @toggle-todo="toggleTodo"
-                  @delete-todo="deleteTodo" />
-    
-        <br>
-        <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li
-              v-if="currentPage !== 1" 
-              class="page-item">
-              <a class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
-            </li>
-            <li
-              v-for="page in numberOfPages"
-              :key="page"
-              class="page-item"
-              :class="currentPage === page ? 'active' : ''">
-              <a class="page-link"
-                 @click="getTodos(page)">
-                 {{page}}
-              </a>
-            </li>        
-            <li 
-              v-if="numberOfPages != currentPage"
-              class="page-item">
-              <a class="page-link" @click="getTodos(currentPage + 1)">Next</a>
-            </li>
-          </ul>
-        </nav>          
-       
-      </div>   
-    </template>
-    
-    <script>
-    import { ref, computed, watch } from 'vue';
-    import axios from "axios";
-    import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
-    import TodoList from '@/components/TodoList.vue';
-    export default {  
-      components: {
-        TodoSimpleForm,
-        TodoList
-      },
-      setup(){      
-        const todos = ref([]); 
-        const searchText = ref('');
-        const error = ref('');
-        const numberOfTodos = ref(0);
-        const limit = 5;
-        const currentPage = ref(1);
-        let timeout = null;
-    
-        watch(searchText, () => {
-          clearTimeout(timeout);
-          timeout = setTimeout(() => {
-            getTodos(1);
-            }, 2000);
-        });
-    
-        const searchTodo = () => {
-          clearTimeout(timeout);
-          getTodos(1);
-        }
-    
-        const numberOfPages = computed(() => {
-          return Math.ceil(numberOfTodos.value/limit);
-        });   
-    
-        const deleteTodo = async (index) => {
-          error.value = '';
-          const id = todos.value[index].id;
-          try{
-            await axios.delete('http://localhost:3000/todos/'+id);
-            //todos.value.splice(index, 1);
-             getTodos(1);
-          }catch(err){
-            console.log(err);
-            error.value = 'Someting went wrong';
-          } 
-        }   
-    
-        const getTodos = async (page = currentPage.value) => {
-          currentPage.value = page;
-          error.value = '';
-          try{
-            const res = await axios.get(
-              `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`);
-    
-            numberOfTodos.value = res.headers['x-total-count'];         
-            todos.value = res.data;       
-          }catch(err){
-            console.log(err);
-            error.value = 'Someting went wrong';
-          }
-        }
-    
-        getTodos();
-    
-        const addTodo = async (todo) => {
-          error.value = '';
-          try{
-            await axios.post('http://localhost:3000/todos', {
-              subject: todo.subject,
-              completed: todo.completed
-            });
-            //todos.value.push(res.data);
-             getTodos(1);
-          }catch(err){
-            console.log(err);
-            error.value = 'Someting went wrong';
-          }     
-        }
-    
-        const toggleTodo = async (index) => {  
-          error.value = '';
-          const id = todos.value[index].id;
-          try{
-            await axios.patch('http://localhost:3000/todos/'+id, {
-              completed: !todos.value[index].completed
-            });
-            todos.value[index].completed = !todos.value[index].completed;  
-          }catch(err){
-            console.log(err);
-            error.value = 'Someting went wrong';
-          } 
-        }
-        
-        return{      
-          todos,      
-          deleteTodo,
-          addTodo,
-          toggleTodo, 
-          searchText,
-          error,  
-          getTodos,
-          numberOfTodos,
-          limit,
-          currentPage,
-          numberOfPages, 
-          searchTodo,
-        }
-      }  
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
+
+    <div v-if="!todos.length">
+      There is noting to display  
+    </div>    
+
+    <TodoList :todos="todos"
+              @toggle-todo="toggleTodo"
+              @delete-todo="deleteTodo" />
+
+    <br>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li
+          v-if="currentPage !== 1" 
+          class="page-item">
+          <a class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
+        </li>
+        <li
+          v-for="page in numberOfPages"
+          :key="page"
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''">
+          <a class="page-link"
+             @click="getTodos(page)">
+             {{page}}
+          </a>
+        </li>        
+        <li 
+          v-if="numberOfPages != currentPage"
+          class="page-item">
+          <a class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>          
+   
+  </div>   
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue';
+import axios from "axios";
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
+import TodoList from '@/components/TodoList.vue';
+export default {  
+  components: {
+    TodoSimpleForm,
+    TodoList
+  },
+  setup(){      
+    const todos = ref([]); 
+    const searchText = ref('');
+    const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+    let timeout = null;
+
+    watch(searchText, () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        getTodos(1);
+      }, 2000);      
+    });
+
+    const searchTodo = () => {
+      clearTimeout(timeout);
+      getTodos(1);
     }
-    </script>
-    
-    <style>  
-      .todo{
-        color: gray;
-        text-decoration: line-through;
+
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });   
+
+    const deleteTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.delete('http://localhost:3000/todos/'+id);
+        //todos.value.splice(index, 1);
+         getTodos(1);
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      } 
+    }   
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      error.value = '';
+      try{
+        const res = await axios.get(
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`);
+
+        numberOfTodos.value = res.headers['x-total-count'];         
+        todos.value = res.data;       
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
       }
-    </style>
+    }
+
+    getTodos();
+
+    const addTodo = async (todo) => {
+      error.value = '';
+      try{
+        await axios.post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed
+        });
+        //todos.value.push(res.data);
+         getTodos(1);
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      }     
+    }
+
+    const toggleTodo = async (index, checked) => {  
+      console.log(checked);
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.patch('http://localhost:3000/todos/'+id, {
+          completed: !todos.value[index].completed
+        });
+        todos.value[index].completed = checked;  
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      } 
+    }
     
+    return{      
+      todos,      
+      deleteTodo,
+      addTodo,
+      toggleTodo, 
+      searchText,
+      error,  
+      getTodos,
+      numberOfTodos,
+      limit,
+      currentPage,
+      numberOfPages,
+      searchTodo, 
+    }
+  }  
+}
+</script>
+
+<style>  
+  .todo{
+    color: gray;
+    text-decoration: line-through;
+  }
+</style>
